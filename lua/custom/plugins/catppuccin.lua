d1d0c9676e187800f0a61f277e01b8fdde74236d
Ranger11Danger <ranger11danger@necromancerlabs.com>
2024-05-08 20:12:38 -0400

    Updated settings

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -10,7 +10,6 @@ return
       flash = true,
       gitsigns = true,
       illuminate = true,
-      indent_blankline = { enabled = true },
       lsp_trouble = true,
       mason = true,
       mini = true,
--- lua/custom/plugins/settings.lua
+++ lua/custom/plugins/settings.lua
@@ -1,4 +1,8 @@
 vim.o.number = true
-vim.o .relativenumber = true
+vim.o.relativenumber = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.shiftwidth = 4
+vim.opt.scrolloff = 8
 
 return {}
